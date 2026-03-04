c5afa98a10297c8e14494de98bb4d318dad5d036
Elias Bewig <melone5467@gmail.com>
2026-03-04 13:23:39 +0100

    configured karma conf to see better error results

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -27,7 +27,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'text'}
       ],
       check: {
         global: {
