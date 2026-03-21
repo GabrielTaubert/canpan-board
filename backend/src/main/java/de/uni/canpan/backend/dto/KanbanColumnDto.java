b5ac92289de79dd348e25581770e3c799894e95b
Gabriel Taubert <gabrieltaubert03@gmail.com>
2026-03-22 00:47:20 +0100

    add isSystem to dto

--- backend/src/main/java/de/uni/canpan/backend/dto/KanbanColumnDto.java
+++ backend/src/main/java/de/uni/canpan/backend/dto/KanbanColumnDto.java
@@ -1,5 +1,6 @@
 package de.uni.canpan.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import de.uni.canpan.backend.model.KanbanColumn;
 
 import java.util.UUID;
@@ -7,13 +8,16 @@ import java.util.UUID;
 public record KanbanColumnDto(
         UUID id,
         String name,
-        int position
+        int position,
+        @JsonProperty("isSystem")
+        boolean isSystem
 ) {
     public static KanbanColumnDto from(KanbanColumn column) {
         return new KanbanColumnDto(
                 column.getId(),
                 column.getName(),
-                column.getPosition()
+                column.getPosition(),
+                column.isSystem()
         );
     }
 }
