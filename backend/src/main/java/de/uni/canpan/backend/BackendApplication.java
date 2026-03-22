570eb9fa17bdf7953c9f362e9e56cda5e05b60e4
Gabriel Taubert <gabrieltaubert03@gmail.com>
2026-03-22 14:55:16 +0100

    enable scheduling for automatic archiving

--- backend/src/main/java/de/uni/canpan/backend/BackendApplication.java
+++ backend/src/main/java/de/uni/canpan/backend/BackendApplication.java
@@ -4,9 +4,11 @@ import de.uni.canpan.backend.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class BackendApplication {
 
 	public static void main(String[] args) {
